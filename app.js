2cb29b7e169da0efe48921dede386308439f9fa1
Student <Student@Mac-mini-2.local>
2016-03-14 16:51:47 -0600

    beginning materialize

--- app.js
+++ app.js
@@ -1,143 +1,144 @@
-//--------------Start Food Items-------------\\
-function FoodItem(name, calories, vegan, glutenFree, citrusFree) {
-  this.name = name
-  this.calories = calories
-  this.vegan = vegan
-  this.glutenFree = glutenFree
-  this.citrusFree = citrusFree
-}
-FoodItem.prototype.stringify = function() {
-    return (this.name.toUpperCase() + ' has ' + this.calories + ' calories.\n' + 'Vegan: ' + this.vegan + '\nGluten free: ' + this.glutenFree + '\nCitrus Free: ' + this.citrusFree)
-  }
-//Food
-var salad   = new FoodItem('Salad', 200, true, true, false)
-var steak   = new FoodItem('Steak', 700, false, true, true)
-var brownie = new FoodItem('Brownie', 1500, false, false, true)
+(function() {
+  'use strict';
+  angular.module('meatNBooze', [])
+    .controller('meatController', meatController)
+
+  function meatController() {
+    var meatCtrl = this
+
+    //--------------Start Food Items-------------\\
+    function FoodItem(name, calories, vegan, glutenFree, citrusFree) {
+      this.name = name
+      this.calories = calories
+      this.vegan = vegan
+      this.glutenFree = glutenFree
+      this.citrusFree = citrusFree
+    }
+    FoodItem.prototype.stringify = function() {
+        return this.name.toUpperCase() + ' has ' + this.calories + ' calories.\n' + 'Vegan: ' + this.vegan + '\nGluten free: ' + this.glutenFree + '\nCitrus Free: ' + this.citrusFree
+      }
+      //Food
+    meatCtrl.salad = new FoodItem('Salad', 200, true, true, false)
+    meatCtrl.steak = new FoodItem('Steak', 700, false, true, true)
+    meatCtrl.brownie = new FoodItem('Brownie', 1500, false, false, true)
 
 
 
-//--------------------Drinks-------------------\\
-function Drink(name, description, price, ingredients) {
-  this.name = name
-  this.description = description
-  this.price = price
-  this.ingredients = ingredients
-}
-Drink.prototype.stringify = function() {
+    //--------------------Drinks-------------------\\
+    function Drink(name, description, price, ingredients) {
+      this.name = name
+      this.description = description
+      this.price = price
+      this.ingredients = ingredients
+    }
+    Drink.prototype.stringify = function() {
 
-  return (this.name.toUpperCase() + ' is a ' + this.description + '.\nPrice: $' + this.price + '\nIngredients: ' + this.ingredients)
-}
-//Drink
-var oldFashioned = new Drink('Old Fashioned', 'classic cocktail', 8, 'Whiskey, sugar, bitters')
+        return (this.name.toUpperCase() + ' is a ' + this.description + '.\nPrice: $' + this.price + '\nIngredients: ' + this.ingredients)
+      }
+      //Drink
+    meatCtrl.oldFashioned = new Drink('Old Fashioned', 'classic cocktail', 8, 'Whiskey, sugar, bitters')
 
 
 
-//--------------------Plates-------------------\\
-function Plate(name, description, price, ingredients) {
-  this.name = name
-  this.description = description
-  this.price = price
-  this.ingredients = ingredients
-}
-Plate.prototype.stringify = function() {
-  var stringArray = []
-  for (var i = 0; i < this.ingredients.length; i++) {
-    stringArray.push(this.ingredients[i].name)
-  }
-  var foodItemsNames = stringArray.join(', ')
-  return (this.name.toUpperCase() + ' is ' + this.description + '.\nPrice: $' + this.price + '\nIngredients: ' + foodItemsNames)
-}
-Plate.prototype.isVegan = function() {
-  for (var i = 0; i < this.ingredients.length; i++) {
-    if (this.ingredients[i].vegan == false) {
-      return false
+    //--------------------Plates-------------------\\
+    function Plate(name, description, price, ingredients) {
+      this.name = name
+      this.description = description
+      this.price = price
+      this.ingredients = ingredients
+    }
+    Plate.prototype.stringify = function() {
+      var stringArray = []
+      for (var i = 0; i < this.ingredients.length; i++) {
+        stringArray.push(this.ingredients[i].name)
+      }
+      var foodItemsNames = stringArray.join(', ')
+      return (this.name.toUpperCase() + ' is ' + this.description + '.\nPrice: $' + this.price + '\nIngredients: ' + foodItemsNames)
+    }
+    Plate.prototype.isVegan = function() {
+      for (var i = 0; i < this.ingredients.length; i++) {
+        if (this.ingredients[i].vegan == false) {
+          return false
+        }
+      }
+      return true
+    }
+    Plate.prototype.isGlutenFree = function() {
+      for (var i = 0; i < this.ingredients.length; i++) {
+        if (this.ingredients[i].glutenFree == false) {
+          return false
+        }
+      }
+      return true
+    }
+    Plate.prototype.isCitrusFree = function() {
+        for (var i = 0; i < this.ingredients.length; i++) {
+          if (this.ingredients[i].citrusFree == false) {
+            return false
+          }
+        }
+        return true
+      }
+      //Plates
+    meatCtrl.plate1 = new Plate('MWF Special', 'fresh salad with chocolatey dessert', 12, [meatCtrl.salad, meatCtrl.brownie])
+    meatCtrl.plate2 = new Plate('T/TH Special', 'fresh salad and steak', 14, [meatCtrl.salad, meatCtrl.steak])
+    meatCtrl.plate3 = new Plate('Weekend Special', 'drinking time', 8, [meatCtrl.oldFashioned])
+    meatCtrl.platesArray = [meatCtrl.plate1, meatCtrl.plate2, meatCtrl.plate3]
+
+
+
+    //---------------------Order---------------------\\
+    function Order(plates) {
+      this.plates = plates
+    }
+    Order.prototype.stringify = function() {
+      for (var i = 0; i < this.plates.length; i++) {
+        console.log(this.plates[i].stringify())
+      }
+    }
+    meatCtrl.order1 = new Order(meatCtrl.platesArray)
+      // console.log("Calling order1.stringify()")
+      // order1.stringify()
+
+
+
+    //---------------------Menu---------------------\\
+    function Menu(plates) {
+      this.plates = plates
+    }
+    Menu.prototype.stringify = function() {
+      for (var i = 0; i < this.plates.length; i++) {
+        console.log(this.plates[i].stringify())
+      }
+    }
+    meatCtrl.menu1 = new Menu(meatCtrl.platesArray)
+      // console.log("Calling menu1.stringify()")
+      // menu1.stringify()
+
+
+
+    //------------------Restaurant------------------\\
+    function Restaurant(name, description, menu) {
+      this.name = name
+      this.description = description
+      this.menu = menu
+    }
+    Restaurant.prototype.stringify = function() {
+      console.log(this.name.toUpperCase() + ': ' + this.description + '.')
+      for (var i = 0; i < this.menu.plates.length; i++) {
+        console.log(this.menu.plates[i].stringify())
+      }
+    }
+    meatCtrl.meatNBooze = new Restaurant("Meat n' Booze", 'The name says it all', meatCtrl.menu1)
+      // console.log("Calling RestauranteFantastico.stringify()")
+      // RestauranteFantastico.stringify()
+
+
+
+    //------------------Customer------------------\\
+    function Customer(dietaryPreference) {
+      this.dietaryPreference = dietaryPreference
     }
   }
-  return true
-}
-Plate.prototype.isGlutenFree = function() {
-  for (var i = 0; i < this.ingredients.length; i++) {
-    if (this.ingredients[i].glutenFree == false) {
-      return false
-    }
-  }
-  return true
-}
-Plate.prototype.isCitrusFree = function() {
-  for (var i = 0; i < this.ingredients.length; i++) {
-    if (this.ingredients[i].citrusFree == false) {
-      return false
-    }
-  }
-  return true
-}
-//Plates
-var plate1 = new Plate('MWF Special',  'fresh salad with chocolatey dessert', 12, [salad, brownie])
-var plate2 = new Plate('T/TH Special', 'fresh salad and steak', 14, [salad, steak])
-var plate3 = new Plate('Weekend Special', 'drinking time', 8, [oldFashioned])
-var platesArray = [plate1, plate2, plate3]
 
-
-
-//---------------------Order---------------------\\
-function Order(plates) {
-  this.plates = plates
-}
-Order.prototype.stringify = function() {
-  for (var i = 0; i < this.plates.length; i++) {
-    console.log(this.plates[i].stringify())
-  }
-}
-var order1 = new Order(platesArray)
-  // console.log("Calling order1.stringify()")
-  // order1.stringify()
-
-
-
-//---------------------Menu---------------------\\
-function Menu(plates) {
-  this.plates = plates
-}
-Menu.prototype.stringify = function() {
-  for (var i = 0; i < this.plates.length; i++) {
-    console.log(this.plates[i].stringify())
-  }
-}
-var menu1 = new Menu(platesArray)
-  // console.log("Calling menu1.stringify()")
-  // menu1.stringify()
-
-
-
-//------------------Restaurant------------------\\
-function Restaurant(name, description, menu) {
-  this.name = name
-  this.description = description
-  this.menu = menu
-}
-Restaurant.prototype.stringify = function() {
-  console.log(this.name.toUpperCase() + ': ' + this.description + '.')
-  for (var i = 0; i < this.menu.plates.length; i++) {
-    console.log(this.menu.plates[i].stringify())
-  }
-}
-var meatNBooze = new Restaurant("Meat n' Booze", 'The name says it all', menu1)
-  // console.log("Calling RestauranteFantastico.stringify()")
-  // RestauranteFantastico.stringify()
-
-
-
-//------------------Customer------------------\\
-function Customer(dietaryPreference) {
-  this.dietaryPreference = dietaryPreference
-}
-
-
-
-
-
-
-
-
-
-//
+}());
